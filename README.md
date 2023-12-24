892faa28eb0fd69de54dc25ed9c19fd2992fbfc8
Apep <154458667+apep-ix@users.noreply.github.com>
2023-12-24 14:01:15 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,29 @@
+# passwdgen
+
+passwdgen is a simple command-line tool for generating random passwords with customizable length, characters, digits, and special characters.
+
+## Usage
+
+
+python passwdgen.py -l <length> -c <characters> -n <digits> -s <special_characters>  
+
+# Arguments
+
+    -l: total length of the password must be between 6-256 inclusive (required).
+    -c: number of characters in the password (use 0 for no characters) (required).
+    -n: number of digits in the password (use 0 for no digits) (required).
+    -s: number of special characters in the password (use 0 for no special ch) (required).
+    
+# Examples
+
+Generate a password with length 12, including 4 characters, 6 digits, and 2 special characters:
+
+
+python passwdgen.py -l 12 -c 4 -n 6 -s 2
+
+Generate a password with only characters (no digits or special characters):
+
+
+python passwdgen.py -l 8 -c 8 -n 0 -s 0
+
+
---
+++ passwdgen.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import argparse
+import secrets
+import string
+import random
+
+def main():
+    parser = argparse.ArgumentParser(prog='passwdgen', description='generate random password')
+
+    parser.add_argument('-l', '--len', type=int, required=True, metavar='', help='total password length must be betwen 6-256 (inclusive)')
+    parser.add_argument('-c', '--char', type=int, required=True, metavar='', help='number of characters')
+    parser.add_argument('-n', '--num', type=int, required=True, metavar='', help='number of digits')
+    parser.add_argument('-s', '--spec', type=int, required=True, metavar='', help='number of special characters')
+
+    args = parser.parse_args()
+
+    if args.len > 256 or args.len <= 0:
+        raise ValueError('Total password length must be betwen 6-256 (inclusive)')
+    elif args.char + args.num + args.spec > args.len or args.char + args.num + args.spec  < args.len:
+        raise ValueError('The sum of character set lengths does not match the specified length')
+    elif args.char < 0 or args.num < 0 or args.spec < 0:
+        raise ValueError('Value must be greater than or equal to 0')
+    
+    gen_passwd(args.len, args.char, args.num, args.spec)
+               
+def gen_passwd(l, c, n, s):
+    characters = ''.join(secrets.choice(string.ascii_letters) for _ in range(c)) 
+    numbers = ''.join(secrets.choice(string.digits) for _ in range(n))
+    special = ''.join(secrets.choice(string.punctuation) for _ in range(s))
+
+    passwd_list = [characters, numbers, special]
+    secrets.SystemRandom().shuffle(passwd_list)
+    passwd = ''.join(passwd_list)
+    shuffled_passwd = ''.join(random.sample(passwd, len(passwd)))
+    print(f'\nPassword: {shuffled_passwd}')
+
+if __name__ == '__main__':
+    main()
